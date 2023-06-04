e5c72f7aeab66535b849bd37dce46f1e2650f61e
NikaA3 <abuashvili.nika@yahoo.com>
2023-06-04 14:44:41 +0400

    base functionality made

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,69 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
+import Head from "next/head";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MeetupDetails = (props) => {
+  return (
+    <>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetupIdObject = new ObjectId(meetupId);
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: meetupIdObject,
+  });
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  };
+}
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+export default HomePage;
